05825e7b2e9d2bc53c3acccc1e105e29703cf350
AdityaNathK <aditya.nath@aligent.com.au>
2022-09-07 23:37:05 +0930

    React old code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import "app.scss";
-import Button from "components/atoms/Button/Button";
+import { useState, useLayoutEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React Application</h1>
-    </div>
-  );
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [flag, setFlag] = useState(false);
+
+    function handleClick() {
+        console.log("=== click ===");
+        fetchSomething().then(() => {
+            setCount((c) => c + 1); // Causes a re-render
+            setFlag((f) => !f); // Causes a re-render
+        });
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>Next</button>
+            <h1 style={{ color: flag ? "blue" : "black" }}>{count}</h1>
+            <LogEvents />
+        </div>
+    );
+};
+
+function LogEvents(props) {
+    useLayoutEffect(() => {
+        console.log("Commit");
+    });
+    console.log("Render");
+    return null;
+}
+
+function fetchSomething() {
+    return new Promise((resolve) => setTimeout(resolve, 100));
 }
 
 export default App;
